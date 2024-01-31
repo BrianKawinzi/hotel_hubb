72baf7c361a4f806dd817423c36c734183d85cf9
BrianKawinzi <brian.mutisya@strathmore.edu>
2024-01-31 09:36:54 +0300

    bar chart added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hotel_hubb/pages/home_page.dart';
-import 'package:hotel_hubb/pages/intro_page.dart';
+
 
 void main() {
   runApp(const MyApp());
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: sized_box_for_whitespace
+
 import 'package:flutter/material.dart';
+import 'package:hotel_hubb/bargraph/individual_bar.dart';
 import 'package:hotel_hubb/widgets/navigation_drawer.dart';
 import 'package:hotel_hubb/widgets/progress_indicator.dart';
 
@@ -99,7 +102,7 @@ class _HomePageState extends State<HomePage> {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
 
-              //sales tile
+              //sales card
               Card(
                 elevation: 5.0,
                 child: Padding(
@@ -142,14 +145,109 @@ class _HomePageState extends State<HomePage> {
                         Colors.yellow, 
                         'Target Sales', 
                         82, 
-                        Color.fromARGB(255, 38, 0, 255),
+                        const Color.fromARGB(255, 38, 0, 255),
                       ),
 
                       const SizedBox(height: 10),
                     ],
                   ),
                 ),
-              )
+              ),
+
+              const SizedBox(height: 10),
+
+              //revenue card
+              Card(
+                elevation: 5.0,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'Total \n Revenue',
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.grey,
+                            ),
+                          ),
+
+
+                          //calculation of monthly amounts
+                          FutureBuilder<double>(
+                            future: calculateTotalRevenue(),
+                            builder: (context, snaphot) {
+                              if (snaphot.hasData) {
+                                return Text(
+                                  'KSH \n ${snaphot.data!.toStringAsFixed(0)}',
+                                  style: const TextStyle(
+                                    fontSize: 19.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                );
+                              } else {
+                                return const Text(
+                                  'Loading...',
+                                  style: TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                );
+                              }
+                            },
+                          ),
+
+                          DropdownButton<String>(
+                            items: <String>[
+                              '2022',
+                              '2023',
+                              '2024',
+                              '2025',
+                              '2026',
+                              '2027'
+                            ].map((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                            onChanged: (String? newValue) {
+                              //Handle dropdown value change logic
+                            },
+                            value: '2024',
+                          ),
+                        ],
+                      ),
+
+                      //Barchart
+                      Container(
+                        height: 200,
+                        child: const IndividualBar(
+                          x: 1, 
+                          monthlyAmounts: [
+                            10000,
+                            20000,
+                            15000,
+                            25000,
+                            18000,
+                            22000,
+                            30500,
+                            28000,
+                            35000,
+                            32000,
+                            28000,
+                            40000
+                          ]
+                        ),
+                      ),
+                      
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
